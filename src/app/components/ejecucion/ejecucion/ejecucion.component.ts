d62ed301109cb13d9e7d10d1ff42134cf6e68083
Brayan Alonzo <brayan.quialo@gmail.com>
2022-05-14 18:48:11 -0600

    Se eliminaron clases innecesarias Consola.ts y ReporteErrores.ts

--- src/app/components/ejecucion/ejecucion/ejecucion.component.ts
+++ src/app/components/ejecucion/ejecucion/ejecucion.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Consola } from 'src/backend/front/Consola';
 import { ManejadorEjecucion } from '../../../../backend/front/ManejadorEjecucion';
 
 @Component({
--- src/app/components/ejecucion/terminal/terminal.component.ts
+++ src/app/components/ejecucion/terminal/terminal.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Consola } from 'src/backend/front/Consola';
 
 @Component({
   selector: 'app-terminal',
