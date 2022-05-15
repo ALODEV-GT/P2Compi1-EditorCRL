9d666f4f1068cef129d0f01183602622c27e4309
Brayan Alonzo <brayan.quialo@gmail.com>
2022-05-14 20:38:30 -0600

    Se optimizo codigo para sumar valores primitivos

--- src/backend/back/ejecucion/Operaciones/Suma.ts
+++ src/backend/back/ejecucion/Operaciones/Suma.ts
@@ -26,199 +26,208 @@ export class Suma extends Instruccion {
             return;
         }
 
-        //Boolean + Boolean
-        if (exp1 instanceof Boolean && exp2 instanceof Boolean) {
-            let valor1: number = exp1.valorNumerico();
-            let valor2: number = exp2.valorNumerico();
-            let suma = valor1 + valor2;
-            return new Decimal(suma, this._linea);
+        if (exp1 instanceof Boolean) {
+            //Boolean + Boolean
+            if (exp2 instanceof Boolean) {
+                let valor1: number = exp1.valorNumerico();
+                let valor2: number = exp2.valorNumerico();
+                let suma = valor1 + valor2;
+                return new Decimal(suma, this._linea);
+            }
+
+            //Boolean + Double
+            if (exp2 instanceof Decimal) {
+                let valor1: number = exp1.valorNumerico();
+                let valor2: number = exp2.valor_1;
+                let suma = valor1 + valor2;
+                return new Decimal(suma, this._linea);
+            }
+
+            //Boolean + String
+            if (exp2 instanceof Cadena) {
+                let valor1: string = exp1.valorString();
+                let valor2: string = exp2.valor;
+                let suma = valor1 + valor2;
+                return new Cadena(suma, this._linea);
+            }
+
+            //Boolean + Int
+            if (exp2 instanceof Entero) {
+                let valor1: number = exp1.valorNumerico();
+                let valor2: number = exp2.valor_1;
+                let suma = valor1 + valor2;
+                return new Entero(suma, this._linea);
+            }
+
+            //Boolean + Char
+            if (exp2 instanceof Char) {
+                Errores.getInstance().push(new Error("Sintactico", this._linea, "No se puede sumar Boolean con un Char"));
+            }
         }
 
-        //Boolean + Double
-        if (exp1 instanceof Boolean && exp2 instanceof Decimal) {
-            let valor1: number = exp1.valorNumerico();
-            let valor2: number = exp2.valor_1;
-            let suma = valor1 + valor2;
-            return new Decimal(suma, this._linea);
+        if (exp1 instanceof Decimal) {
+            //Double + Boolean
+            if (exp2 instanceof Boolean) {
+                let valor1: number = exp1.valor_1;
+                let valor2: number = exp2.valorNumerico();
+                let suma = valor1 + valor2;
+                return new Decimal(suma, this._linea);
+            }
+
+            //Double + Double
+            if (exp2 instanceof Decimal) {
+                let valor1: number = exp1.valor_1;
+                let valor2: number = exp2.valor_1;
+                let suma = valor1 + valor2;
+                return new Decimal(suma, this._linea);
+            }
+
+            //Double + String
+            if (exp2 instanceof Cadena) {
+                let valor1: number = exp1.valor_1;
+                let valor2: string = exp2.valor;
+                let suma = valor1 + valor2;
+                return new Cadena(suma, this._linea);
+            }
+
+            //Double + Int
+            if (exp2 instanceof Entero) {
+                let valor1: number = exp1.valor_1;
+                let valor2: number = exp2.valor_1;
+                let suma = valor1 + valor2;
+                return new Decimal(suma, this._linea);
+            }
+
+            //Double + Char
+            if (exp2 instanceof Char) {
+                let valor1: number = exp1.valor_1;
+                let valor2: number = exp2.valorNumerico();
+                let suma = valor1 + valor2;
+                return new Decimal(suma, this._linea);
+            }
         }
 
-        //Boolean + String
-        if (exp1 instanceof Boolean && exp2 instanceof Cadena) {
-            let valor1: string = exp1.valorString();
-            let valor2: string = exp2.valor;
-            let suma = valor1 + valor2;
-            return new Cadena(suma, this._linea);
+        if (exp1 instanceof Cadena) {
+            //String + Boolean
+            if (exp2 instanceof Boolean) {
+                let valor1: string = exp1.valor;
+                let valor2: string = exp2.valorString();
+                let suma = valor1 + valor2;
+                return new Cadena(suma, this._linea);
+            }
+
+            //String + Double
+            if (exp2 instanceof Decimal) {
+                let valor1: string = exp1.valor;
+                let valor2: number = exp2.valor_1;
+                let suma = valor1 + valor2;
+                return new Cadena(suma, this._linea);
+            }
+
+            //String + String
+            if (exp2 instanceof Cadena) {
+                let valor1: string = exp1.valor;
+                let valor2: string = exp2.valor;
+                let suma = valor1 + valor2;
+                return new Cadena(suma, this._linea);
+            }
+
+            //String + Int
+            if (exp2 instanceof Entero) {
+                let valor1: string = exp1.valor;
+                let valor2: number = exp2.valor_1;
+                let suma = valor1 + valor2;
+                return new Cadena(suma, this._linea);
+            }
+
+            //String + Char
+            if (exp2 instanceof Char) {
+                let valor1: string = exp1.valor;
+                let valor2: string = exp2.valor;
+                let suma = valor1 + valor2;
+                return new Cadena(suma, this._linea);
+            }
         }
 
-        //Boolean + Int
-        if (exp1 instanceof Boolean && exp2 instanceof Entero) {
-            let valor1: number = exp1.valorNumerico();
-            let valor2: number = exp2.valor_1;
-            let suma = valor1 + valor2;
-            return new Entero(suma, this._linea);
+        if (exp1 instanceof Entero) {
+            //Int + Boolean
+            if (exp2 instanceof Boolean) {
+                let valor1: number = exp1.valor_1;
+                let valor2: number = exp2.valorNumerico();
+                let suma = valor1 + valor2;
+                return new Entero(suma, this._linea);
+            }
+
+            //Int + Double
+            if (exp2 instanceof Decimal) {
+                let valor1: number = exp1.valor_1;
+                let valor2: number = exp2.valor_1;
+                let suma = valor1 + valor2;
+                return new Decimal(suma, this._linea);
+            }
+
+            //Int + String
+            if (exp2 instanceof Cadena) {
+                let valor1: number = exp1.valor_1;
+                let valor2: string = exp2.valor;
+                let suma = valor1 + valor2;
+                return new Cadena(suma, this._linea);
+            }
+
+            //Int + Int
+            if (exp2 instanceof Entero) {
+                let valor1: number = exp1.valor_1;
+                let valor2: number = exp2.valor_1;
+                let suma = valor1 + valor2;
+                return new Entero(suma, this._linea);
+            }
+
+            //Int + Char
+            if (exp2 instanceof Char) {
+                let valor1: number = exp1.valor_1;
+                let valor2: number = exp2.valorNumerico();
+                let suma = valor1 + valor2;
+                return new Entero(suma, this._linea);
+            }
         }
 
-        //Boolean + Char
-        if (exp1 instanceof Boolean && exp2 instanceof Char) {
-            Errores.getInstance().push(new Error("Sintactico", this._linea, "No se puede sumar Boolean con un Char"));
-        }
+        if (exp1 instanceof Char) {
+            //Char + Boolean
+            if (exp2 instanceof Boolean) {
+                Errores.getInstance().push(new Error("Sintactico", this._linea, "No se puede sumar Char con un Boolean"));
+            }
 
-        //Double + Boolean
-        if (exp1 instanceof Decimal && exp2 instanceof Boolean) {
-            let valor1: number = exp1.valor_1;
-            let valor2: number = exp2.valorNumerico();
-            let suma = valor1 + valor2;
-            return new Decimal(suma, this._linea);
-        }
+            //Char + Double
+            if (exp2 instanceof Decimal) {
+                let valor1: number = exp1.valorNumerico();
+                let valor2: number = exp2.valor_1;
+                let suma = valor1 + valor2;
+                return new Decimal(suma, this._linea);
+            }
 
-        //Double + Double
-        if (exp1 instanceof Decimal && exp2 instanceof Decimal) {
-            let valor1: number = exp1.valor_1;
-            let valor2: number = exp2.valor_1;
-            let suma = valor1 + valor2;
-            return new Decimal(suma, this._linea);
-        }
+            //Char + String
+            if (exp2 instanceof Cadena) {
+                let valor1: string = exp1.valor;
+                let valor2: string = exp2.valor;
+                let suma = valor1 + valor2;
+                return new Cadena(suma, this._linea);
+            }
 
-        //Double + String
-        if (exp1 instanceof Decimal && exp2 instanceof Cadena) {
-            let valor1: number = exp1.valor_1;
-            let valor2: string = exp2.valor;
-            let suma = valor1 + valor2;
-            return new Cadena(suma, this._linea);
-        }
+            //Char + Int
+            if (exp2 instanceof Entero) {
+                let valor1: number = exp1.valorNumerico();
+                let valor2: number = exp2.valor_1;
+                let suma = valor1 + valor2;
+                return new Entero(suma, this._linea);
+            }
 
-        //Double + Int
-        if (exp1 instanceof Decimal && exp2 instanceof Entero) {
-            let valor1: number = exp1.valor_1;
-            let valor2: number = exp2.valor_1;
-            let suma = valor1 + valor2;
-            return new Decimal(suma, this._linea);
-        }
-
-        //Double + Char
-        if (exp1 instanceof Decimal && exp2 instanceof Char) {
-            let valor1: number = exp1.valor_1;
-            let valor2: number = exp2.valorNumerico();
-            let suma = valor1 + valor2;
-            return new Decimal(suma, this._linea);
-        }
-
-        //String + Boolean
-        if (exp1 instanceof Cadena && exp2 instanceof Boolean) {
-            let valor1: string = exp1.valor;
-            let valor2: string = exp2.valorString();
-            let suma = valor1 + valor2;
-            return new Cadena(suma, this._linea);
-        }
-
-        //String + Double
-        if (exp1 instanceof Cadena && exp2 instanceof Decimal) {
-            let valor1: string = exp1.valor;
-            let valor2: number = exp2.valor_1;
-            let suma = valor1 + valor2;
-            return new Cadena(suma, this._linea);
-        }
-
-        //String + String
-        if (exp1 instanceof Cadena && exp2 instanceof Cadena) {
-            let valor1: string = exp1.valor;
-            let valor2: string = exp2.valor;
-            let suma = valor1 + valor2;
-            return new Cadena(suma, this._linea);
-        }
-
-        //String + Int
-        if (exp1 instanceof Cadena && exp2 instanceof Entero) {
-            let valor1: string = exp1.valor;
-            let valor2: number = exp2.valor_1;
-            let suma = valor1 + valor2;
-            return new Cadena(suma, this._linea);
-        }
-
-        //String + Char
-        if (exp1 instanceof Cadena && exp2 instanceof Char) {
-            let valor1: string = exp1.valor;
-            let valor2: string = exp2.valor;
-            let suma = valor1 + valor2;
-            return new Cadena(suma, this._linea);
-        }
-
-
-        //Int + Boolean
-        if (exp1 instanceof Entero && exp2 instanceof Boolean) {
-            let valor1: number = exp1.valor_1;
-            let valor2: number = exp2.valorNumerico();
-            let suma = valor1 + valor2;
-            return new Entero(suma, this._linea);
-        }
-
-        //Int + Double
-        if (exp1 instanceof Entero && exp2 instanceof Decimal) {
-            let valor1: number = exp1.valor_1;
-            let valor2: number = exp2.valor_1;
-            let suma = valor1 + valor2;
-            return new Decimal(suma, this._linea);
-        }
-
-        //Int + String
-        if (exp1 instanceof Entero && exp2 instanceof Cadena) {
-            let valor1: number = exp1.valor_1;
-            let valor2: string = exp2.valor;
-            let suma = valor1 + valor2;
-            return new Cadena(suma, this._linea);
-        }
-
-        //Int + Int
-        if (exp1 instanceof Entero && exp2 instanceof Entero) {
-            let valor1: number = exp1.valor_1;
-            let valor2: number = exp2.valor_1;
-            let suma = valor1 + valor2;
-            return new Entero(suma, this._linea);
-        }
-
-        //Int + Char
-        if (exp1 instanceof Entero && exp2 instanceof Char) {
-            let valor1: number = exp1.valor_1;
-            let valor2: number = exp2.valorNumerico();
-            let suma = valor1 + valor2;
-            return new Entero(suma, this._linea);
-        }
-
-        //Char + Boolean
-        if (exp1 instanceof Char && exp2 instanceof Boolean) {
-            Errores.getInstance().push(new Error("Sintactico", this._linea, "No se puede sumar Char con un Boolean"));
-        }
-
-        //Char + Double
-        if (exp1 instanceof Char && exp2 instanceof Decimal) {
-            let valor1: number = exp1.valorNumerico();
-            let valor2: number = exp2.valor_1;
-            let suma = valor1 + valor2;
-            return new Decimal(suma, this._linea);
-        }
-
-        //Char + String
-        if (exp1 instanceof Char && exp2 instanceof Cadena) {
-            let valor1: string = exp1.valor;
-            let valor2: string = exp2.valor;
-            let suma = valor1 + valor2;
-            return new Cadena(suma, this._linea);
-        }
-
-        //Char + Int
-        if (exp1 instanceof Char && exp2 instanceof Entero) {
-            let valor1: number = exp1.valorNumerico();
-            let valor2: number = exp2.valor_1;
-            let suma = valor1 + valor2;
-            return new Entero(suma, this._linea);
-        }
-
-        //Char + Char
-        if (exp1 instanceof Char && exp2 instanceof Char) {
-            let valor1: number = exp1.valorNumerico();
-            let valor2: number = exp2.valorNumerico();
-            let suma = valor1 + valor2;
-            return new Entero(suma, this._linea);
+            //Char + Char
+            if (exp2 instanceof Char) {
+                let valor1: number = exp1.valorNumerico();
+                let valor2: number = exp2.valorNumerico();
+                let suma = valor1 + valor2;
+                return new Entero(suma, this._linea);
+            }
         }
 
         return;
