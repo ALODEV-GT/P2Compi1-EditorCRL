7ee6ce9ee7a359e0a44dc1dc746ca1b055aaeda3
Brayan Alonzo <brayan.quialo@gmail.com>
2022-05-16 00:07:10 -0600

    Se agrego la ejecucion para asignar, y asignar con casteo

--- src/backend/back/ejecucion/Declaraciones/Asignacion.ts
+++ src/backend/back/ejecucion/Declaraciones/Asignacion.ts
@@ -1,5 +1,13 @@
 import { Entorno } from '../Entorno';
 import { Instruccion } from '../Instruccion';
+import { Errores } from '../Errores/Errores';
+import { Error } from '../Errores/Error';
+import { Cadena } from '../Valores/Cadena';
+import { Decimal } from '../Valores/Decimal';
+import { Boolean } from '../Valores/Boolean';
+import { Entero } from '../Valores/Entero';
+import { Char } from '../Valores/Char';
+import { TiposNativos } from './TiposNativo';
 export class Asignacion extends Instruccion {
     private _id: string
     private _exp: Instruccion
@@ -11,6 +19,93 @@ export class Asignacion extends Instruccion {
     }
 
     ejecutar(e: Entorno) {
-        throw new Error('Method not implemented.');
+        //Busqueda de id
+        const variable = e.getVariable(this._id);
+        if (!variable) {
+            Errores.getInstance().push(new Error('semantico', this._linea, `La variable ${this._id} no esta declarada`));
+            return;
+        }
+
+        let valor = this._exp.ejecutar(e);
+        let id = valor.id;
+        //CASTEOS
+        if (variable.tipo == TiposNativos.STRING) {
+            if (valor instanceof Cadena) {
+                //sin acciones
+            } else if (valor instanceof Decimal) {
+                valor = new Cadena(valor.valor_1.toString(), this._linea);
+            } else if (valor instanceof Boolean) {
+                valor = new Cadena(valor.valorNumerico().toString(), this._linea);
+            } else if (valor instanceof Entero) {
+                valor = new Cadena(valor.valor_1.toString(), this._linea);
+            } else if (valor instanceof Char) {
+                valor = new Cadena(valor.valor, this._linea);
+            }
+        } else if (variable.tipo == TiposNativos.DOUBLE) {
+            if (valor instanceof Cadena) {
+                Errores.getInstance().push(new Error('semantico', this._linea, `No se puede asignar un String a la variable Double ${id}`));
+                return
+            } else if (valor instanceof Decimal) {
+                //Sin acciones
+            } else if (valor instanceof Boolean) {
+                valor = new Decimal(valor.valorNumerico(), this._linea);
+            } else if (valor instanceof Entero) {
+                valor = new Decimal(valor.valor_1, this._linea);
+            } else if (valor instanceof Char) {
+                valor = new Decimal(valor.valorNumerico(), this._linea)
+            }
+        } else if (variable.tipo == TiposNativos.BOOLEAN) {
+            if (valor instanceof Cadena) {
+                Errores.getInstance().push(new Error('semantico', this._linea, `No se puede asignar un String a la variable Boolean ${id}`));
+                return
+            } else if (valor instanceof Decimal) {
+                Errores.getInstance().push(new Error('semantico', this._linea, `No se puede asignar un Double a la variable Boolean ${id}`));
+                return
+            } else if (valor instanceof Boolean) {
+                //Sin acciones
+            } else if (valor instanceof Entero) {
+                Errores.getInstance().push(new Error('semantico', this._linea, `No se puede asignar un Int a la variable Boolean ${id}`));
+                return
+            } else if (valor instanceof Char) {
+                Errores.getInstance().push(new Error('semantico', this._linea, `No se puede asignar un Char a la variable Boolean ${id}`));
+                return
+            }
+        } else if (variable.tipo == TiposNativos.INT) {
+            if (valor instanceof Cadena) {
+                Errores.getInstance().push(new Error('semantico', this._linea, `No se puede asignar un String a la variable Int ${id}`));
+                return
+            } else if (valor instanceof Decimal) {
+                let conv = ~~(valor.valor_1);
+                valor = new Entero(conv, this._linea);
+            } else if (valor instanceof Boolean) {
+                valor = new Entero(valor.valorNumerico(), this._linea);
+            } else if (valor instanceof Entero) {
+                //sin accion
+            } else if (valor instanceof Char) {
+                valor = new Entero(valor.valorNumerico(), this._linea);
+            }
+        } else if (variable.tipo == TiposNativos.CHAR) {
+            if (valor instanceof Cadena) {
+                Errores.getInstance().push(new Error('semantico', this._linea, `No se puede asignar un String a la variable Char ${id}`));
+                return
+            } else if (valor instanceof Decimal) {
+                Errores.getInstance().push(new Error('semantico', this._linea, `No se puede asignar un Double a la variable Char ${id}`));
+                return
+            } else if (valor instanceof Boolean) {
+                Errores.getInstance().push(new Error('semantico', this._linea, `No se puede asignar un Boolean a la variable Char ${id}`));
+                return
+            } else if (valor instanceof Entero) {
+                let conv = String.fromCharCode(valor.valor_1);
+                valor = new Char(conv, this._linea);
+            } else if (valor instanceof Char) {
+                //sin accion
+            }
+        } else {
+            Errores.getInstance().push(new Error('semantico', this._linea, `No se puede asignar un tipo diferente a la variable ${id}`));
+            return;
+        }
+
+        variable.valor = valor;
+        console.log("Reasignacion realizada");
     }
 }
--- src/backend/back/ejecucion/Declaraciones/Variable.ts
+++ src/backend/back/ejecucion/Declaraciones/Variable.ts
@@ -4,6 +4,7 @@ import { Instruccion } from '../Instruccion';
 export class Variable {
     private _id: string;
     private _tipo: TiposNativos;
+   
     valor: Instruccion | null = null;
 
     constructor(id: string, tipo: TiposNativos, valor?: Instruccion | null) {
@@ -17,6 +18,12 @@ export class Variable {
     }
     public set id(value: string) {
         this._id = value;
+    }
+    public get tipo(): TiposNativos {
+        return this._tipo;
+    }
+    public set tipo(value: TiposNativos) {
+        this._tipo = value;
     }
 
 }
