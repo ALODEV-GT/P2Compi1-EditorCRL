b704b331e9e37896b392fc09b5ed2e6e7d32236e
Brayan Alonzo <brayan.quialo@gmail.com>
2022-05-16 00:35:14 -0600

    Se agrego la ejecucion a la instruccion Mientras

--- src/backend/back/ejecucion/Instrucciones/Mientras.ts
+++ src/backend/back/ejecucion/Instrucciones/Mientras.ts
@@ -1,5 +1,11 @@
 import { Entorno } from '../Entorno';
 import { Instruccion } from '../Instruccion';
+import { Boolean } from '../Valores/Boolean';
+import { Errores } from '../Errores/Errores';
+import { Error } from '../Errores/Error';
+import { Retorno } from '../flujo/Retorno';
+import { Detener } from '../flujo/Detener';
+import { Continuar } from '../flujo/Continuar';
 export class Mientras extends Instruccion {
     private _condicion: Instruccion;
     private _instrucciones: Array<Instruccion>;
@@ -11,6 +17,32 @@ export class Mientras extends Instruccion {
     }
 
     ejecutar(e: Entorno) {
-        throw new Error('Method not implemented.');
+        const condicion = this._condicion.ejecutar(e);
+        if(!(condicion instanceof Boolean)){
+            Errores.getInstance().push(new Error('semantico', this._linea, `Condicion invalida`));
+            return;
+        }
+
+        while ((this._condicion.ejecutar(e) as Boolean).valor_1) {
+            //Creacion de entorno
+            const entorno = new Entorno(e);
+            //Ejecucion de instrucciones
+            for (let instruccion of this._instrucciones) {
+                const resp = instruccion.ejecutar(entorno);
+                ///Validacion de instruccion Return
+                if (resp instanceof Retorno) {
+                    return resp;
+                }
+                //Validacion de instrucion Break
+                if (resp instanceof Detener) {
+                    return;
+                }
+                //Validacion de instruccion Continue
+                if (resp instanceof Continuar) {
+                    break;
+                }
+            }
+        }
+        return
     }
 }
